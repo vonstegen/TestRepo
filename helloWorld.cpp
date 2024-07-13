fd374c4a29e57466ee05916f383ecc6a6e08430d
vonstegen <vonstegen@pm.me>
2024-07-12 22:34:10 -0400

    Commit CPP Files

---
+++ helloWorld.cpp
@@ -0,0 +1,15 @@
+// C++ program to display "Hello World!"
+
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+
+// Main() function: where the execution of
+// program begins
+int main()
+{
+	// Prints Hello World!
+	cout << "Hello World!\n";
+
+	return 0;
+}
